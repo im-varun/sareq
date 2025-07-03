cc5f1df46c6957150e9bf766dbd9e0c275c1edfa
im-varun <vrun.mulchandani@gmail.com>
2025-07-02 18:30:33 -0600

    Add version flag to root command

--- cmd/root.go
+++ cmd/root.go
@@ -4,12 +4,22 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/im-varun/sareq/internal/utils"
 	"github.com/spf13/cobra"
 )
+
+var versionFlag bool
 
 var rootCmd = &cobra.Command{
 	Use:   "sareq",
 	Short: "SAReq is a CLI-based HTTP client for modern developers",
+	Run: func(cmd *cobra.Command, args []string) {
+		if versionFlag {
+			fmt.Println(utils.VersionString())
+		} else {
+			cmd.Help()
+		}
+	},
 }
 
 func Execute() {
@@ -17,4 +27,8 @@ func Execute() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+}
+
+func init() {
+	rootCmd.Flags().BoolVarP(&versionFlag, "version", "v", false, "Prints the version of SAReq in use")
 }
