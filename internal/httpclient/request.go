a8201da0ccc34af6369733c2c435ce73a4e5b1b7
im-varun <vrun.mulchandani@gmail.com>
2025-07-11 20:09:12 -0500

    Add build logic for request config

--- internal/httpclient/request.go
+++ internal/httpclient/request.go
@@ -1,5 +1,11 @@
 package httpclient
 
+import (
+	"io"
+	"net/http"
+	"strings"
+)
+
 type RequestConfig struct {
 	method string
 	url    string
@@ -12,4 +18,20 @@ func NewRequestConfig(method string, url string, body string) *RequestConfig {
 		url:    url,
 		body:   body,
 	}
+}
+
+func (rc *RequestConfig) Build() (*http.Request, error) {
+	method := strings.ToUpper(rc.method)
+
+	var body io.Reader
+	if rc.body != "" {
+		body = strings.NewReader(rc.body)
+	}
+
+	req, err := http.NewRequest(method, rc.url, body)
+	if err != nil {
+		return nil, err
+	}
+
+	return req, nil
 }
