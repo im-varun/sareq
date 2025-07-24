c1df7c12c9acb79ac1b5bb9d7e72dcb144a3d302
im-varun <vrun.mulchandani@gmail.com>
2025-07-24 18:27:44 -0500

    Add basic test cases for ValidateRequestURL()

--- internal/httpclient/utils_test.go
+++ internal/httpclient/utils_test.go
@@ -10,24 +10,42 @@ func TestValidateRequestURL(t *testing.T) {
 		expectedError  string
 	}{
 		{
-			name:           "Empty Request URL",
+			name:           "empty request url",
 			reqURL:         "",
 			expectedOutput: "",
 			expectedError:  "request URL cannot be empty",
+		},
+		{
+			name:           "empty request url with whitespaces",
+			reqURL:         "    ",
+			expectedOutput: "",
+			expectedError:  "failed to parse request URL",
+		},
+		{
+			name:           "empty request url with tab spaces",
+			reqURL:         "				",
+			expectedOutput: "",
+			expectedError:  "failed to parse request URL",
+		},
+		{
+			name:           "empty request url with whitespaces and tab spaces",
+			reqURL:         "  			  		",
+			expectedOutput: "",
+			expectedError:  "failed to parse request URL",
 		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			answer, err := ValidateRequestURL(test.reqURL)
+			output, err := ValidateRequestURL(test.reqURL)
 			if err != nil {
 				if err.Error() != test.expectedError {
 					t.Errorf("expected error: \"%s\", got error: \"%s\"", test.expectedError, err.Error())
 				}
 			}
 
-			if answer != test.expectedOutput {
-				t.Errorf("expected output: \"%s\", got output: \"%s\"", test.expectedOutput, answer)
+			if output != test.expectedOutput {
+				t.Errorf("expected output: \"%s\", got output: \"%s\"", test.expectedOutput, output)
 			}
 		})
 	}
