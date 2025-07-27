7ad70520ca0b2b9aa6363f7da618b3e3a9e6d691
im-varun <vrun.mulchandani@gmail.com>
2025-07-27 11:11:49 -0500

    Add common run function for http requests and reuse it in request commands

--- cmd/patch.go
+++ cmd/patch.go
@@ -2,7 +2,6 @@ package cmd
 
 import (
 	"github.com/im-varun/sareq/cmd/flags"
-	"github.com/im-varun/sareq/internal/httpclient"
 	"github.com/spf13/cobra"
 )
 
@@ -12,21 +11,7 @@ var patchCmd = &cobra.Command{
 	Short:   "Send HTTP PATCH request to the specified URL",
 	Args:    cobra.ExactArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
-		reqURL, err := httpclient.ValidateRequestURL(args[0])
-		if err != nil {
-			return err
-		}
-
-		if flags.ReqBody != "" {
-			err = httpclient.ValidateRequestBody(flags.ReqBody)
-			if err != nil {
-				return err
-			}
-		}
-
-		client := httpclient.NewClient(flags.ReqTimeout)
-
-		err = client.Do(cmd.Name(), reqURL, flags.ReqBody)
+		err := runRequest(cmd.Name(), args[0])
 
 		return err
 	},
---
+++ cmd/utils.go
@@ -0,0 +1,26 @@
+package cmd
+
+import (
+	"github.com/im-varun/sareq/cmd/flags"
+	"github.com/im-varun/sareq/internal/httpclient"
+)
+
+func runRequest(reqMethod string, reqURL string) error {
+	reqURL, err := httpclient.ValidateRequestURL(reqURL)
+	if err != nil {
+		return err
+	}
+
+	if flags.ReqBody != "" {
+		err = httpclient.ValidateRequestBody(flags.ReqBody)
+		if err != nil {
+			return err
+		}
+	}
+
+	client := httpclient.NewClient(flags.ReqTimeout)
+
+	err = client.Do(reqMethod, reqURL, flags.ReqBody)
+
+	return err
+}
