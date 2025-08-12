eac7413097b38f88ad6e7b316f9c54642067984b
im-varun <vrun.mulchandani@gmail.com>
2025-08-12 18:14:09 -0500

    Move response struct printer utils to a printer package

--- cli/cliutil/httprunner/runner.go
+++ cli/cliutil/httprunner/runner.go
@@ -1,11 +1,7 @@
 package httprunner
 
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"strings"
-
+	"github.com/im-varun/sareq/cli/cliutil/httpprinter"
 	"github.com/im-varun/sareq/cli/flags"
 	"github.com/im-varun/sareq/internal/httpclient"
 )
@@ -30,59 +26,7 @@ func Run(reqMethod string, reqURL string) error {
 		return err
 	}
 
-	printResponse(resp)
+	httpprinter.PrintResponse(resp)
 
 	return nil
-}
-
-func printResponse(r *httpclient.Response) {
-	fmt.Println(r.Protocol(), r.Status())
-	fmt.Println()
-
-	header := r.Header()
-	for key, values := range header {
-		if len(values) == 1 {
-			fmt.Printf("%s: %s\n", key, values[0])
-		} else {
-			fmt.Printf("%s: %v\n", key, values)
-		}
-	}
-	fmt.Println()
-
-	body := strings.Trim(r.Body(), "\r\n")
-
-	var bodyIsJSONType bool
-
-	contentType, ok := header["Content-Type"]
-	if ok {
-		for _, str := range contentType {
-			if strings.Contains(str, "application/json") {
-				bodyIsJSONType = true
-				break
-			}
-		}
-	}
-
-	if bodyIsJSONType {
-		prettyBody, err := prettifyResponseBody(body)
-		if err != nil {
-			// body could not be prettifyed, so printing it normally
-			fmt.Println(body)
-		} else {
-			fmt.Println(prettyBody)
-		}
-	} else {
-		fmt.Println(body)
-	}
-}
-
-func prettifyResponseBody(respBody string) (string, error) {
-	var prettyBody bytes.Buffer
-
-	err := json.Indent(&prettyBody, []byte(respBody), "", "  ")
-	if err != nil {
-		return "", err
-	}
-
-	return prettyBody.String(), nil
 }
