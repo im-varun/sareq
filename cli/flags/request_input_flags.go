b09ec2297335ef3c12c2860f4e73d2a07b472bff
im-varun <vrun.mulchandani@gmail.com>
2025-08-16 16:10:32 -0500

    Add basic flag setup for http request header

--- cli/flags/request_input_flags.go
+++ cli/flags/request_input_flags.go
@@ -19,12 +19,20 @@ const (
 	reqBodyUsage   string = "set body to send with HTTP request"
 )
 
+const (
+	reqHeaderLong  string = "header"
+	reqHeaderShort string = "H"
+	reqHeaderUsage string = "set header to sent with HTTP request"
+)
+
 var ReqTimeout int
 var ReqBody string
+var ReqHeader map[string]string
 
 func RegisterRequestFlags(reqCmd *cobra.Command) {
 	reqCmd.Flags().IntVarP(&ReqTimeout, reqTimeoutLong, reqTimeoutShort, reqTimeoutDefault, reqTimeoutUsage)
 	reqCmd.Flags().StringVarP(&ReqBody, reqBodyLong, reqBodyShort, reqBodyDefault, reqBodyUsage)
+	reqCmd.Flags().StringToStringVarP(&ReqHeader, reqHeaderLong, reqHeaderShort, nil, reqHeaderUsage)
 
 	commandName := reqCmd.Name()
 	if commandName == "post" || commandName == "put" || commandName == "patch" {
