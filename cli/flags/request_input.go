8e05aa27b65956f8f965fda52ede9e9446b33298
im-varun <vrun.mulchandani@gmail.com>
2025-08-17 12:08:06 -0500

    Remove constants for flags

--- cli/flags/request_input.go
+++ cli/flags/request_input.go
@@ -5,37 +5,17 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const (
-	reqTimeoutLong    string = "timeout"
-	reqTimeoutShort   string = "t"
-	reqTimeoutDefault int    = httpclient.DefaultTimeoutSeconds
-	reqTimeoutUsage   string = "set timeout for HTTP request"
-)
-
-const (
-	reqBodyLong    string = "body"
-	reqBodyShort   string = "B"
-	reqBodyDefault string = ""
-	reqBodyUsage   string = "set body to send with HTTP request"
-)
-
-const (
-	reqHeaderLong  string = "header"
-	reqHeaderShort string = "H"
-	reqHeaderUsage string = "set header to sent with HTTP request"
-)
-
 var ReqTimeout int
 var ReqBody string
 var ReqHeader map[string]string
 
 func RegisterRequestFlags(reqCmd *cobra.Command) {
-	reqCmd.Flags().IntVarP(&ReqTimeout, reqTimeoutLong, reqTimeoutShort, reqTimeoutDefault, reqTimeoutUsage)
-	reqCmd.Flags().StringVarP(&ReqBody, reqBodyLong, reqBodyShort, reqBodyDefault, reqBodyUsage)
-	reqCmd.Flags().StringToStringVarP(&ReqHeader, reqHeaderLong, reqHeaderShort, nil, reqHeaderUsage)
+	reqCmd.Flags().IntVarP(&ReqTimeout, "timeout", "t", httpclient.DefaultTimeoutSeconds, "set timeout for HTTP request")
+	reqCmd.Flags().StringVarP(&ReqBody, "body", "B", "", "set body to send with HTTP request")
+	reqCmd.Flags().StringToStringVarP(&ReqHeader, "header", "H", nil, "set header to send with HTTP request")
 
 	commandName := reqCmd.Name()
 	if commandName == "post" || commandName == "put" || commandName == "patch" {
-		reqCmd.MarkFlagRequired(reqBodyLong)
+		reqCmd.MarkFlagRequired("body")
 	}
 }
--- cli/flags/response_formatting.go
+++ cli/flags/response_formatting.go
@@ -2,22 +2,10 @@ package flags
 
 import "github.com/spf13/cobra"
 
-const (
-	respNoColorLong    string = "no-color"
-	respNoColorDefault bool   = false
-	respNoColorUsage   string = "disable coloring for HTTP response"
-)
-
-const (
-	respNoPrettifyLong    string = "no-prettify"
-	respNoPrettifyDefault bool   = false
-	respNoPrettifyUsage   string = "disable prettification for HTTP response"
-)
-
 var RespNoColor bool
 var RespNoPrettify bool
 
 func RegisterResponseFormattingFlags(reqCmd *cobra.Command) {
-	reqCmd.Flags().BoolVar(&RespNoColor, respNoColorLong, respNoColorDefault, respNoColorUsage)
-	reqCmd.Flags().BoolVar(&RespNoPrettify, respNoPrettifyLong, respNoPrettifyDefault, respNoPrettifyUsage)
+	reqCmd.Flags().BoolVar(&RespNoColor, "no-color", false, "disable coloring for HTTP response")
+	reqCmd.Flags().BoolVar(&RespNoPrettify, "no-prettify", false, "disable prettification for HTTP response")
 }
