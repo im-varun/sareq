10f259eaf514229cd826eb537d9d95c2268c3279
im-varun <vrun.mulchandani@gmail.com>
2025-09-06 18:22:50 -0500

    Remove shorthand for request timeout flag

--- cli/flags/request_input.go
+++ cli/flags/request_input.go
@@ -18,9 +18,9 @@ var reqHeader map[string]string
 
 // RegisterRequestFlags registers HTTP request related flags with the specified request command.
 func RegisterRequestFlags(reqCmd *cobra.Command) {
-	reqCmd.Flags().StringVarP(&reqBody, "body", "B", "", "set body to send with HTTP request (e.g '{\"key1\": int, \"key2\": \"string\"}')")
-	reqCmd.Flags().StringToStringVarP(&reqHeader, "header", "H", nil, "set header to send with HTTP request (e.g key=value or \"key=value\")")
-	reqCmd.Flags().IntVarP(&reqTimeout, "timeout", "t", httpclient.DefaultTimeoutSeconds, "set timeout for HTTP request")
+	reqCmd.Flags().StringVarP(&reqBody, "body", "B", "", "set the body to send with HTTP request (e.g '{\"key1\": int, \"key2\": \"string\"}')")
+	reqCmd.Flags().StringToStringVarP(&reqHeader, "header", "H", nil, "set a header to send with HTTP request (e.g \"key=value\")")
+	reqCmd.Flags().IntVar(&reqTimeout, "timeout", httpclient.DefaultTimeoutSeconds, "set timeout for HTTP request")
 
 	commandName := reqCmd.Name()
 	if commandName == "post" || commandName == "put" || commandName == "patch" {
